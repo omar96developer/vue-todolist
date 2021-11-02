531ac27448716441c2bcd65893cd386f26bf69ec
omar96developer <omakino@hotmail.it>
2021-11-02 16:14:19 +0100

    0.2

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ var app = new Vue(
         },
         methods: {
             onAddClick(){
+                //se elemento e vuoto non lo aggiungo alla lista
+                if(this.newTask.trim() === "" ) {
+                    return;
+                }
+                //verifico se elemento da aggungere è presente nella lista se si lo push
+                const sameTask = this.taskToDo.some((el) => {
+                    return el.toLowerCase() === this.newTask.trim().toLowerCase();
+                });
+                if(sameTask) {
+                    return;
+                }
                 this.taskToDo.push(this.newTask);
             }
         }
